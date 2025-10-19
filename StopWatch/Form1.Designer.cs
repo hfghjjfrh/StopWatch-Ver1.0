6813af48373ffe9041641f7cfdb617b528f6d5f5
hfghjjfrh <hfghjjfrh@gmail.com>
2025-10-19 18:58:15 +0900

    Add files via upload

---
+++ StopWatch/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace StopWatch
+{
+    partial class StopWatch
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.title = new System.Windows.Forms.Label();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Start = new System.Windows.Forms.Button();
+            this.display = new System.Windows.Forms.Label();
+            this.Reset = new System.Windows.Forms.Button();
+            this.Status = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.Font = new System.Drawing.Font("HG正楷書体-PRO", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.title.Location = new System.Drawing.Point(68, 26);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(553, 97);
+            this.title.TabIndex = 0;
+            this.title.Text = "Stop Watch";
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.Stop.Font = new System.Drawing.Font("MS UI Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Stop.Location = new System.Drawing.Point(331, 291);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(309, 122);
+            this.Stop.TabIndex = 1;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Start.Font = new System.Drawing.Font("MS UI Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Start.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.Start.Location = new System.Drawing.Point(12, 291);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(299, 122);
+            this.Start.TabIndex = 2;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // display
+            // 
+            this.display.Font = new System.Drawing.Font("MS UI Gothic", 84.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.display.Location = new System.Drawing.Point(121, 135);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(442, 109);
+            this.display.TabIndex = 3;
+            this.display.Text = "00:00:00";
+            // 
+            // Reset
+            // 
+            this.Reset.BackColor = System.Drawing.Color.Black;
+            this.Reset.Font = new System.Drawing.Font("MS UI Gothic", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.Reset.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.Reset.Location = new System.Drawing.Point(170, 419);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(309, 122);
+            this.Reset.TabIndex = 4;
+            this.Reset.Text = "Reset";
+            this.Reset.UseVisualStyleBackColor = false;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Status
+            // 
+            this.Status.Location = new System.Drawing.Point(0, 249);
+            this.Status.Name = "Status";
+            this.Status.Size = new System.Drawing.Size(652, 23);
+            this.Status.TabIndex = 5;
+           
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // StopWatch
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(651, 546);
+            this.Controls.Add(this.Status);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.title);
+            this.Font = new System.Drawing.Font("ＭＳ 明朝", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.MaximizeBox = false;
+            this.MaximumSize = this.Size;
+            this.MinimumSize = this.Size;
+            this.Name = "StopWatch";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "StopWatch Ver1.0";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label display;
+        private System.Windows.Forms.Button Reset;
+        private System.Windows.Forms.ProgressBar Status;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ StopWatch/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StopWatch
+{
+    public partial class StopWatch : Form
+    {
+        public StopWatch()
+        {
+            InitializeComponent();
+            _timer1.Interval = 100;
+            _timer1.Tick += timer1_tick;
+            Start.Enabled = true;
+            Stop.Enabled = false;
+            Status.Minimum = 0;
+            Status.Maximum = 0;
+            Status.Value = 0;
+
+        }
+        Timer _timer1 = new Timer();
+        TimeSpan _timespan = new TimeSpan(0);
+
+      
+        private void timer1_tick(object sender, EventArgs e)
+        {
+                TimeSpan timespan = DateTime.Now - sn + _timespan;
+                display.Text = string.Format("{0:00}:{1:00}:{2:00}", timespan.Hours, timespan.Minutes, timespan.Seconds);
+        }    
+        DateTime sn = DateTime.Now;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeSpan timespan = DateTime.Now - sn + _timespan;
+            display.Text = string.Format("{0:00}:{1:00}:{2:00}", timespan.Hours, timespan.Minutes, timespan.Seconds);
+        }
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Start.Enabled = false;
+            Stop.Enabled = true;
+           
+            
+            try
+            {
+                SoundPlayer simpleSound = new SoundPlayer(@"..\..\sound\chimes.wav");
+                simpleSound.Play();
+            }
+            catch(System.IO.FileNotFoundException) 
+            {
+                MessageBox.Show("サウンドファイルが見つかりません。\nファイルが指定の場所にあるか確認してください。\n","エラー");
+            }
+            _timer1.Start();
+            sn = DateTime.Now;
+            Status.Style = ProgressBarStyle.Marquee;
+            Status.Maximum = 10;
+            Status.MarqueeAnimationSpeed = 30;
+        }
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer simpleSound = new SoundPlayer(@"..\..\sound\chimes.wav");
+                simpleSound.Play();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("サウンドファイルが見つかりません。\nファイルが指定の場所にあるか確認してください。\n", "エラー");
+            }
+
+            Start.Enabled = true;
+            _timer1.Stop();
+            _timespan += DateTime.Now - sn;
+            Start.Enabled = true;
+            Stop.Enabled = false;
+            Status.Style = ProgressBarStyle.Marquee;
+            Status.MarqueeAnimationSpeed = 0;
+
+            
+
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SoundPlayer simpleSound = new SoundPlayer(@"..\..\sound\notify.wav");
+                simpleSound.Play();
+            }
+            catch (System.IO.FileNotFoundException)
+            {
+                MessageBox.Show("サウンドファイルが見つかりません。\nファイルが指定の場所にあるか確認してください。\n", "エラー");
+            }
+
+            Start.Enabled = true;
+            Stop.Enabled = false;
+            Status.Minimum = 0;
+            Status.Maximum = 0;
+            Status.Value = 0;
+            _timer1.Stop();
+            _timespan = new TimeSpan(0);
+            sn = DateTime.Now;
+            display.Text = string.Format("{0:00}:{1:00}:{2:00}", _timespan.Hours, _timespan.Minutes, _timespan.Seconds);
+        }
+
+        
+    }
+}
